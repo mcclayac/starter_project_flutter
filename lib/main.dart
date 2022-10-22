59e8749b61a3ddcac8d794c1ef3e66b143e2b040
Anthony McClay <anthony.mcclay@gmail.com>
2022-10-22 07:05:11 -0500

    Added Text

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); /*This is added */
 }
 
 // #docregion MyApp
